549ac598815feea21790814a10b43339a2b907a5
Ankur Agarwal <ankuronetwo@gmail.com>
2021-08-19 23:41:32 -0400

    function descriptions added.

--- util/loop.py
+++ util/loop.py
@@ -8,6 +8,7 @@ from util.utils import accuracy, plotter, TemperatureAnnealing
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def log(filename, logger, debug):
+    """Logging the experiment results as log files."""
     level = logging.DEBUG if debug else logging.INFO
     logging.basicConfig(level=level)
     if filename: 
@@ -20,7 +21,27 @@ def log(filename, logger, debug):
 
 def train(data, model, epochs, batch_size, optimizer, 
           validate=True, print_epochs=False, plot=True, logfile=None, checkpoint_val_best=None):
+    """Trains a model for classification task using negative log likelihood loss for a model returning log(softmax) predictions.
 
+    Args: 
+        data: The data dictionary to train and/or validate.
+        model: The model to be used for training and/or validate.
+        epochs: Total number of iterations.
+        batch_size: The batch_size of training samples.
+        optimizer: The optimizer object.
+        validate: True for doing validation every epoch. False for not validating.
+        print_epochs: True for printing the epochs.  False to run silently.
+        plot: True to plot training and/or validation accuracy and loss curve.
+        logfile: None by default resulting in no log file creation. Creates a log file with the filename passed.
+        checkpoint_val_best: None by default returns final iteration values. Else,
+                                "loss": Returns the model state and validation accuracy for lowest validation loss.
+                                "accuracy": Returns the model state and validation accuracy for highest validation accuracy.
+
+    Returns:
+
+        model_best: The model state as per checkpoint_val_best value.
+        display_valid_acc: The required validation accuracy value to be returned.
+    """
     logger = logging.getLogger(__name__)
     logger = log(logfile, logger, print_epochs)
     model = model.to(device)
@@ -116,6 +137,29 @@ def train(data, model, epochs, batch_size, optimizer,
 
 def student_train(Data, Student_Model, epochs, batch_size_ratio, optimizer, alpha=0.5, T=1.0,
                   validate=True, print_epochs=False, plot=True, logfile=None, checkpoint_val_best=None):
+    """Trains a student model for classification task using cross entropy loss and knowledge distillation loss.
+
+    Args: 
+        Data: The data dictionary to train and/or validate. It should have the teacher infered logits too.
+        Student_Model: The student model to be used for training and/or validate.
+        epochs: Total number of iterations.
+        batch_size_ratio: The ratio of the total data to be used as batch size for training.
+        optimizer: The optimizer object.
+        alpha: The alpha weighting of soft labels. Between [0,1]
+        T: The temperature value for softening the logits for the soft label training.
+        validate: True for doing validation every epoch. False for not validating.
+        print_epochs: True for printing the epochs.  False to run silently.
+        plot: True to plot training and/or validation accuracy and loss curve.
+        logfile: None by default resulting in no log file creation. Creates a log file with the filename passed.
+        checkpoint_val_best: None by default returns final iteration values. Else,
+                                "loss": Returns the model state and validation accuracy for lowest validation loss.
+                                "accuracy": Returns the model state and validation accuracy for highest validation accuracy.
+
+    Returns:
+
+        Student_Model_best: The model state as per checkpoint_val_best value.
+        display_valid_acc: The required validation accuracy value to be returned.
+    """
 
     logger = logging.getLogger(__name__)
     logger = log(logfile, logger, print_epochs)
@@ -234,9 +278,32 @@ def student_train(Data, Student_Model, epochs, batch_size_ratio, optimizer, alph
 def student_train_KD_annealing(Data, Student_Model, epochs, batch_size_ratio, optimizer, alpha=0.5, T_max=1.0,
                   validate=True, print_epochs=False, plot=True, logfile=None, checkpoint_val_best=None):
     """
+    Trains a student model for classification task using knowledge distillation annealing technique.
+
     Ref: Jafari, A., Rezagholizadeh, M., Sharma, P. and Ghodsi, A., 2021, April. Annealing Knowledge Distillation. 
          In Proceedings of the 16th Conference of the European Chapter of the Association for Computational Linguistics: 
          Main Volume (pp. 2493-2504).
+
+    Args: 
+        Data: The data dictionary to train and/or validate. It should have the teacher infered logits too.
+        Student_Model: The student model to be used for training and/or validate.
+        epochs: Total number of iterations.
+        batch_size_ratio: The ratio of the total data to be used as batch size for training.
+        optimizer: The optimizer object.
+        alpha: The alpha weighting of soft labels. Between [0,1]
+        T_max: The max temperature value for softening the logits for the soft label training.
+        validate: True for doing validation every epoch. False for not validating.
+        print_epochs: True for printing the epochs.  False to run silently.
+        plot: True to plot training and/or validation accuracy and loss curve.
+        logfile: None by default resulting in no log file creation. Creates a log file with the filename passed.
+        checkpoint_val_best: None by default returns final iteration values. Else,
+                                "loss": Returns the model state and validation accuracy for lowest validation loss.
+                                "accuracy": Returns the model state and validation accuracy for highest validation accuracy.
+
+    Returns:
+
+        Student_Model_best: The model state as per checkpoint_val_best value.
+        display_valid_acc: The required validation accuracy value to be returned.
     """
     logger = logging.getLogger(__name__)
     logger = log(logfile, logger, print_epochs)
--- util/model.py
+++ util/model.py
@@ -6,12 +6,20 @@ class FFNN(nn.Module):
     """The desired 2 hidden layer feed forward neural network."""
     def __init__(self, random_state=0):
         super(FFNN, self).__init__()
+
+        # Seeding the random weight initialization of the network.
         seeder(random_state)
+
+        # Layer Initialization
         self.hidden1 = torch.nn.Linear(2, 20)
         self.hidden2 = torch.nn.Linear(20, 10)
         self.out = torch.nn.Linear(10,2)
 
     def Z(self, x):
+        """
+        Returns:
+            Logit values on forward pass which need to be passed to the softmax for classification.
+        """
         z = self.hidden1(x)
         z = torch.relu(z)
         z = self.hidden2(z)
@@ -19,6 +27,9 @@ class FFNN(nn.Module):
         return self.out(z)
         
     def forward(self, x):
+        """
+        Returns:
+            Log-softmax values on forward pass to the network.
+        """
         logit = self.Z(x)
         return torch.log_softmax(logit, dim=1)
-        
